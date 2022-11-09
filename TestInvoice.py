1ce9843ae2614cc06a56d1ec9d3317118d9e4166
Leo <drakenwarwolf@gmail.com>
2022-11-09 16:25:31 -0500

    fix products fixture

--- TestInvoice.py
+++ TestInvoice.py
@@ -5,6 +5,7 @@ from Invoice import Invoice
 def products():
     products = {'Pen': {'qnt': 10, 'unit_price': 3.75, 'discount': 5}, 
                 'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
+    return products
 
 @pytest.fixture()
 def invoice():
